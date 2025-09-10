9771268069df967e673f51f03be16accc7564503
Abi7r <abi.k66722@gmail.com>
2025-09-10 18:48:22 +0530

    implemented cocktails Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ gsap.registerPlugin(ScrollTrigger, SplitText)
 import React from 'react'
 import Navbar from './components/navbar.jsx';
 import Hero from './components/hero.jsx';
+import Cocktails from './components/cocktails.jsx';
 const App = () => {
   return (
     <main >
       <Navbar/>
       <Hero/>
-
+  <Cocktails />
     </main>
   );
 };
